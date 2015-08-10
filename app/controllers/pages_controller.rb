87b538e7e7605fbaf60fcf953f63b9c164073561
Baird Castleberry <baird@bairdcastleberry.com>
2015-08-10 18:39:42 -0500

    initial

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,18 @@
+class PagesController < ApplicationController
+
+  def about
+  end
+
+  def howitworks
+  end
+
+  def termsofservice
+  end
+
+  def privacypolicy
+  end
+
+  def contact
+  end
+
+end
