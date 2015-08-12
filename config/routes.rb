f04d004c68c28a3605d9194a4b7f649bf780a166
Baird Castleberry <baird@bairdcastleberry.com>
2015-08-12 13:32:19 -0500

    first and last name hitting db for all model reg

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
   get '/termsofservice' => 'pages#termsofservice'
   get '/privacypolicy' => 'pages#privacypolicy'
 
-  devise_for :users
-  devise_for :sellers
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :sellers, :controllers => { registrations: 'registrations' }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
